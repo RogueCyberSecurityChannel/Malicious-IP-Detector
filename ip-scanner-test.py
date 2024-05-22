37061f7252c6b423887c4b0d06af8b6393e741d9
RogueCyberSecurityChannel <168306032+RogueCyberSecurityChannel@users.noreply.github.com>
2024-05-21 23:12:27 -0600

    Update ip-scanner-test.py
    Added in PID dictionary to give more information to user on malicious process. Going to add full print out PID info.

--- ip-scanner-test.py
+++ ip-scanner-test.py
@@ -1,5 +1,7 @@
 import subprocess
 import requests
+from ipwhois import IPWhois
+import pprint
 #  from requests.exceptions import ConnectionError, RequestException
 import time
 import sys
@@ -20,9 +22,13 @@ def netstat(command):
     try:
         result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
         output_lines = result.stdout.splitlines()
-        raw_ips = [line.split()[2] for line in output_lines if len(line.split()) == 4]
+        raw_ips = [line.split()[2] for line in output_lines if len(line.split()) == 5]
+        raw_pids = [line.split()[4] for line in output_lines if len(line.split()) == 5]
         foreign_ips = []
+        pids = []
+        index2 = 0
         for ip in raw_ips:
+            index2 = index2 + 1
             if "[" in ip:
                 continue
             if "0.0.0.0" in ip:
@@ -32,8 +38,8 @@ def netstat(command):
             if ":" in ip:
                 index = ip.find(":")
                 foreign_ips.append(ip[:index])
-
-        return foreign_ips
+                pids.append(raw_pids[index2 - 1])
+        return foreign_ips, pids
     except subprocess.CalledProcessError:
         pass
 
@@ -52,20 +58,29 @@ def find_matches(driver_list_1, driver_list_2):
     matches = list(set1.intersection(set2))
     return matches
 
+def lists_to_dict(keys, values):
+    return dict(zip(keys, values))
+
 def main():
     data = web_scrape_and_process('https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt')
-    parsed_list = banned_ip_parser(data)
+    parsed_ip_list = banned_ip_parser(data)
 
-    host_ips = netstat('netstat -an')
-    
-    matches = find_matches(parsed_list, host_ips)
+    output = netstat('netstat -ano')
+    host_ips = output[0]
+    host_pids = output[1]
 
+    ip_pid_dictionary = lists_to_dict(host_ips, host_pids)
+
+    matches = find_matches(parsed_ip_list, host_ips)
     match_list = []
     if len(matches):
         for match in matches:
-            print(f"[!] {match}")
+            print(f"[!] ACTIVE CONNECTION TO KNOWN MALICIOUS IP DETECTED")
             match_list.append(match)
     else:
-        print("[+]")
+        print("[+] No active malicious ip connections detected")
+    for match in match_list:
+        print(f'IP: {match} PID: {ip_pid_dictionary[match]}')
+
 if __name__ == "__main__":
     main()
